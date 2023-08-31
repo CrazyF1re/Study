f7268842a960fee706f460d6a445c571586083fa
Vova <vova.plisko@mail.ru>
2023-08-31 15:24:35 +0700

    1

---
+++ win.cpp
@@ -0,0 +1,15 @@
+#include "win.h"
+#include "ui_win.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
